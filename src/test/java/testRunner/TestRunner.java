747c3d5cbcb0a64c4bf885d0ac2328d70e7300af
deepaktechs33 <deepaktechs33@gmail.com>
2026-08-21 14:47:45 +0530

    Fixd fhfbvjhrbvjhjnknkdfdfrj

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -7,10 +7,10 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(
 	//features ="classpath:features/footer.feature",
 				
-		features = "classpath:features/addremovecart.feature",
+		//features = "classpath:features/addremovecart.feature",
 		
 				//features = "src/test/resources/features", 
-				//features = "classpath:features/login.feature",
+				features = "classpath:features/login.feature",
 				//features = "classpath:features/hamberger.feature",
 						//features = "classpath:features/sort.feature",
 								//features = "classpath:features/endtoendflow.feature"	,
