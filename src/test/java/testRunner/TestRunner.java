0056f6c85bde0d67e770e581cdec58acf2008924
deepaktechs33 <deepaktechs33@gmail.com>
2026-08-24 12:58:15 +0530

    addremovecart

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -5,8 +5,8 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features ="classpath:features/footer.feature",
-		//features = "classpath:features/addremovecart.feature",
+		//features ="classpath:features/footer.feature",
+		features = "classpath:features/addremovecart.feature",
         //features = "classpath:features/login.feature",
 		// features = "classpath:features/hamberger.feature",
 		//features = "classpath:features/sort.feature",
